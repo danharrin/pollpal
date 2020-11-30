d308df7c59e1f93cbddd7217eef1cfa37e9e2529
Dan Harrin <git@danharrin.com>
2020-11-30 00:01:37 +0000

    wip

--- index.js
+++ index.js
@@ -75,21 +75,21 @@ client.on('message', msg => {
             msg.react('9️⃣');
             msg.react('🤷');
         } else if (arguments[0] === 'addreaction:') {
-            if (arguments.length < 3) return msg.reply('Please provide the channel ID, message ID, and emoji.');
+            // if (arguments.length < 3) return msg.reply('Please provide the channel ID, message ID, and emoji.');
 
-            let channel = msg.mentions.channels.first() ?? msg.guild.channels.cache.get(arguments[1]);
+            // let channel = msg.mentions.channels.first() ?? msg.guild.channels.cache.get(arguments[1]);
 
-            if (! channel) return msg.reply('Please provide the channel ID.');
+            // if (! channel) return msg.reply('Please provide the channel ID.');
 
-            let message = channel.messages.fetch(arguments[2]);
+            // let message = channel.messages.fetch(arguments[2]);
 
-            if (! message) return msg.reply('Please provide the message ID.');
+            // if (! message) return msg.reply('Please provide the message ID.');
 
-            if (! arguments[3]) return msg.reply('Please provide the emoji.');
+            // if (! arguments[3]) return msg.reply('Please provide the emoji.');
 
-            message.react(arguments[3]);
+            // message.react(arguments[3]);
 
-            msg.reply(`Reacted to the message in ${channel} with ${arguments[3]}.`);
+            // msg.reply(`Reacted to the message in ${channel} with ${arguments[3]}.`);
         };
     } catch (err) {};
 });
